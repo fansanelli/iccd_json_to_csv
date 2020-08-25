c37ce834a56fe9433c5a60cabc81b21ca5729e9f
fansanelli <francians@gmail.com>
2020-08-25 22:16:28 +0200

    contenitori fisici

---
+++ dataset_cf_to_csv.py
@@ -0,0 +1,46 @@
+#!/bin/python
+
+import json
+
+separator = '\t'
+indirizzi = {}
+geometrie = {}
+siti = {}
+
+def convert(path):
+    with open(path) as json_file:
+        data = json.load(json_file)
+        for root in data['@graph']:
+            if root['@type'] == 'clvapit:Address':
+                tmpstr = ''
+                if 'clvapit:fullAddress' in root:
+                    tmpstr = root['clvapit:fullAddress']
+                indirizzi[root['@id']] = tmpstr
+            if root['@type'] == 'clvapit:Geometry':
+                tmpstr = ''
+                if 'clvapit:lat' in root:
+                    tmpstr = root['clvapit:lat'] + ',' + root['clvapit:long']
+                geometrie[root['@id']] = tmpstr
+            if root['@type'] == 'cis:Site':
+                tmpstr = ''
+                if 'cis:siteAddress' in root:
+                    tmpstr = indirizzi[root['cis:siteAddress']['@id']]
+                tmpstr += separator
+                if 'clvapit:hasGeometry' in root:
+                    tmpstr += geometrie[root['clvapit:hasGeometry']['@id']]
+                siti[root['@id']] = tmpstr
+
+        for root in data['@graph']:
+            if root['@type'] == 'cis:CulturalInstituteOrSite':
+                tmpstr = ''
+                if 'cis:hasSite' in root and root['cis:hasSite']['@id'] in siti:
+                    tmpstr = siti[root['cis:hasSite']['@id']]
+                if 'l0:identifier' in root:
+                    print(root['l0:identifier'] + separator + root['cis:institutionalCISName']['@value'] + separator + tmpstr)
+
+def main():
+    convert('dataset-contenitoriFisici.json')
+
+if __name__ == '__main__':
+    main()
+
